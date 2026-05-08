ea28f2c104cbe8eb63f1e703c2452a0b29e1b8f6
davicalebe <davicalebedon@gmail.com>
2026-05-08 11:35:06 -0300

    fix: reorganize schema order

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,12 +1,14 @@
 import prisma from "../src/config/database"
 import { fakerPT_BR as faker } from "@faker-js/faker"
 
+
 const clientes = 10;
+const unidades = 20;
 
 async function main() {
     for (let i = 0; i < clientes; i++) {
         const nome = faker.person.fullName();
-        const email = nome.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase().replace(/ /g, '.') + '@magnumtires.com.br';
+        const email = nome.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase().replace(/ /g, '.') + '@clientes.com.br';
         await prisma.clientes.create({
             data: {
                 nome,
